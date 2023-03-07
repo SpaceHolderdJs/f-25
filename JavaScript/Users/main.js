0efa1fc2f790e9c2e438b304ca514bb623343403
Igor <sergienko339@gmail.com>
2023-03-07 19:59:43 +0200

    users search way 1

--- JavaScript/Users/main.js
+++ JavaScript/Users/main.js
@@ -1,47 +1,72 @@
 const nameInput = document.querySelector("#name");
 const ageInput = document.querySelector("#age");
+const cityInput = document.querySelector("#city");
 const createButton = document.querySelector("#create");
 const usersSection = document.querySelector("#users-section");
+const searchInput = document.querySelector("#search");
+const searchButton = document.querySelector("#search-button");
+const cancelSearchButton = document.querySelector("#cancel-search-button");
 
-let users = [];
+let users = [
+  { name: "Igor", city: "Kyiv", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
+];
+
+renderUsers(users);
 
 const deleteUser = (indexOfUser) => {
-    users = users.filter((el, i) => i !== indexOfUser);
-    renderUsers();
-}
+  users = users.filter((el, i) => i !== indexOfUser);
+  renderUsers(users);
+};
 
-function renderUsers () {
-    
-    usersSection.innerHTML = ""; 
+function renderUsers(usersToRender) {
+  usersSection.innerHTML = "";
 
-    const usersContent = users.map((user) => `<div class="user-card">
+  const usersContent = usersToRender.map(
+    (user) => `<div class="user-card">
         <p>${user.name}</p>
+        <p>${user.city}</p>
         <span>${user.age}</span>
         <button class="delete-user-button">Delete</button>
-    </div>`);
+    </div>`
+  );
 
-     usersContent.forEach((userLayout) => {
-        usersSection.innerHTML += userLayout;
-     });
+  usersContent.forEach((userLayout) => {
+    usersSection.innerHTML += userLayout;
+  });
 
-     const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
+  const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
 
-     deleteButtons.forEach((button, i) => {
-        button.onclick = () => deleteUser(i);
-     });
+  deleteButtons.forEach((button, i) => {
+    button.onclick = () => deleteUser(i);
+  });
 }
 
 createButton.onclick = () => {
   const name = nameInput.value;
   const age = +ageInput.value;
+  const city = cityInput.value;
 
-  const user = {name: name, age: age};
+  const user = { name: name, age: age, city: city };
 
   users.push(user);
 
   nameInput.value = "";
   ageInput.value = "";
+  cityInput.value = "";
 
-  renderUsers();
-}
+  renderUsers(users);
+};
 
+searchButton.onclick = () => {
+  const usersToRender = users.filter((user) =>
+    user.name.includes(searchInput.value)
+  );
+  renderUsers(usersToRender);
+};
+
+cancelSearchButton.onclick = () => {
+    renderUsers(users);
+    searchInput.value = "";
+};
