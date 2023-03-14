981771c83db8e9d3e731fb044e058412152521de
Igor <sergienko339@gmail.com>
2023-03-14 20:20:35 +0200

    pagination

--- JavaScript/Users/main.js
+++ JavaScript/Users/main.js
@@ -6,16 +6,25 @@ const usersSection = document.querySelector("#users-section");
 const searchInput = document.querySelector("#search");
 const sortingByNameCheckbox = document.querySelector("#sort-by-name");
 const sortingByAgeCheckbox = document.querySelector("#sort-by-age");
+const paginationSection = document.querySelector("#pagination");
 
 let users = [
   { name: "Igor", city: "Kyiv", age: 20 },
   { name: "Alex", city: "Kyiv", age: 50 },
   { name: "Oleg", city: "Kyiv", age: 10 },
+  { name: "Igor", city: "Kyiv1", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
+  { name: "Igor", city: "Kyiv2", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
 ];
 
 let changingUser = undefined;
+let paginationpageNumber = 0;
 
 renderUsers(users);
+renderPagination(users.length);
 
 const deleteUser = (indexOfUser) => {
   users = users.filter((el, i) => i !== indexOfUser);
@@ -30,6 +39,21 @@ const editUser = (indexOfUser) => {
   nameInput.value = changingUser.data.name;
   ageInput.value = changingUser.data.age;
   cityInput.value = changingUser.data.city;
+};
+
+function renderPagination (usersQuantity) {
+  for ( let i = 0; i < usersQuantity / 3; i++ ) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.onclick = () => {
+      paginationpageNumber = i;
+      const groupedUsers = groupElementsOfArray(users, 3);
+      renderUsers(groupedUsers[i]);
+    }
+    paginationSection.appendChild(button);
+  }
+  //render first 3 users permanentely 
+  renderUsers(groupElementsOfArray(users, 3)[0]);
 };
 
 const sorting = {
@@ -136,13 +160,94 @@ sortingByAgeCheckbox.onchange = (event) => {
   }
 }
 
+function groupElementsOfArray(arr, oneSetQuantity) {
+  const result = [];
 
-// document.body.addEventListener("click", () => {
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr.slice(i * oneSetQuantity, (i + 1) * oneSetQuantity));
+  }
+
+  return result.filter((arr) => arr.length > 0 );
+}
+
+
+// const onClickBodyEvent = document.body.addEventListener("click", () => {
 //   alert("Hello");
 // });
 
-document.body.onclick = () => {
-  alert("Hello");
-}
+// document.body.removeEventListener("click", onClickBodyEvent);
 
-document.body.onclick = () => {};
+
+// document.body.onclick = () => {
+//   alert("Hello");
+// }
+
+// document.body.onclick = null;
+
+// document.body.ondblclick = () => {
+//   alert("Hello");
+// }
+
+// document.body.onresize = () => {
+//   alert(`Resize ${window.innerWidth} ${window.innerHeight}`);
+  
+// }
+
+
+
+// const newButton = document.createElement("button");
+// newButton.textContent = "NewButton";
+
+// scrollUpButton.replaceWith(newButton);
+
+
+// scrollUpButton.onclick = () => window.print();
+
+
+// document.body.onscroll = () => {
+//   alert(`Scroll ${window.scrollX} ${window.scrollY}`);
+// }
+
+// scrollUpButton.onclick = () => {
+//   window.scrollTo({top: 0, behavior: "smooth"});
+// }
+
+// let interval;
+
+// scrollUpButton.onmousedown = () => {
+//   console.log("mousedown");
+//   interval = setInterval(() => {
+//     window.scrollTo({top: window.scrollY++, behavior: "auto"})}, 1500);
+//     console.log(scrollY);
+// }
+
+// scrollUpButton.onmouseup = () => {
+//   console.log("mouseup");
+//   clearInterval(interval);
+// }
+
+// document.body.oncopy = (e) => {
+//   alert("Copy");
+//   e.preventDefault();
+// }
+
+// document.body.onpaste = () => {
+//   alert("Paste");
+// }
+
+// document.body.onmousemove = (e) => {
+//   console.log(e.pageX, e.pageY, e.target, "EVENT");
+// }
+
+// document.body.oncontextmenu = (e) => {
+//   console.log("right click");
+//   // e.preventDefault();
+// }
+
+// document.body.onkeypress = (e) => {
+//   console.log("Key pressed", e.key);
+// }
+
+// document.body.onkeydown = (e) => {
+//   console.log("Key down", e.key);
+// }
