5c5eaa075de2f8f44a269ebfcfe0f041a8016e86
Igor <sergienko339@gmail.com>
2023-03-30 20:34:56 +0300

    todos

--- JavaScript/Todo/main.js
+++ JavaScript/Todo/main.js
@@ -1,73 +1,128 @@
 const input = document.querySelector("#todo-creation");
 const button = document.querySelector("#create-todo-button");
 const output = document.querySelector("#output");
+const usersOutput = document.querySelector("#users-output");
 
-let todos = [
-    {
-        text: "New text",
-        done: false
-    }
-];
+const isLocalStorageTodosExists = localStorage.getItem("todos");
+
+let todos = isLocalStorageTodosExists
+  ? JSON.parse(isLocalStorageTodosExists)
+  : [];
+
+let users = [];
 
 renderTodos(todos);
 
 button.onclick = () => {
-    const todo = {
-        text: input.value,
-        done: false
-    }
+  const todo = {
+    text: input.value,
+    done: false,
+  };
 
-    input.value = "";
-    
-    todos.push(todo);
+  input.value = "";
 
-    renderTodos(todos);
-}
+  todos.push(todo);
 
-function renderTodos (todosToRender) {
-    output.innerHTML = "";
-    todosToRender.forEach((todo, i) => {
-        output.innerHTML += `
+  renderTodos(todos);
+};
+
+function renderTodos(todosToRender) {
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  output.innerHTML = "";
+  todosToRender.forEach((todo, i) => {
+    output.innerHTML += `
             <div class="todo ${todo.done && "done"}">
-                <span>${i + 1}.</span>
-                <input type="checkbox" ${todo.done && "checked"} class="todo-checkbox" />
-                <span>${todo.text}</span>
+                <div>
+                    <span>${i + 1}.</span>
+                    <input type="checkbox" ${
+                    todo.done && "checked"
+                    } class="todo-checkbox" />
+                    <span>${todo.text}</span>
+                </div>
                 <button class="delete-todo">Delete</button>
             </div>
-        `
-    });
+        `;
+  });
 
-    const checkboxes = [...document.querySelectorAll(".todo-checkbox")];
+  const checkboxes = [...document.querySelectorAll(".todo-checkbox")];
 
-    checkboxes.forEach((checkbox, i) => {
-        checkbox.onchange = () => {
-            const todo = todos[i];
-            changeTodo(todo.text, !todo.done);
-        }
-    });
+  checkboxes.forEach((checkbox, i) => {
+    checkbox.onchange = () => {
+      const todo = todos[i];
+      changeTodo(todo.text, !todo.done);
+    };
+  });
 
-    const deleteButtons = [...document.querySelectorAll(".delete-todo")];
+  const deleteButtons = [...document.querySelectorAll(".delete-todo")];
 
-    deleteButtons.forEach((button, i) => {
-        button.onclick = () => {
-            const todo = todos[i];
-            deleteTodo(todo.text);
-        }
-    });
+  deleteButtons.forEach((button, i) => {
+    button.onclick = () => {
+      const todo = todos[i];
+      deleteTodo(todo.text);
+    };
+  });
 }
 
-function changeTodo (text, newDone) {
-    todos = todos.map((todo) => {
-        if (text === todo.text) {
-            return { text, done: newDone }
-        }
-        return todo;
-    });
+function changeTodo(text, newDone) {
+  todos = todos.map((todo) => {
+    if (text === todo.text) {
+      return { text, done: newDone };
+    }
+    return todo;
+  });
 
-    renderTodos(todos);
+  renderTodos(todos);
 }
 
-function deleteTodo (text) {
+function deleteTodo(text) {
   todos = todos.filter((todo) => todo.text !== text);
   renderTodos(todos);
+}
+
+function getServerTodos () {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((todosFromServer) => {
+      // трансформувати дані відповідним чином: title -> text, completed -> done
+
+      const tranformedTodos = todosFromServer.slice(0, 20).map((todo) => {
+        return {
+          text: todo.title,
+          done: todo.completed,
+          userId: todo.userId,
+          id: todo.id,
+        };
+      });
+
+      console.log(tranformedTodos, "todos");
+
+      todos = tranformedTodos;
+      renderTodos(todos);
+    });
+}
+
+getServerTodos();
+
+function getServerUsers () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(usersFromServer => {
+        console.log(usersFromServer, "users");
+
+        users = usersFromServer;
+        renderUsers();
+    })
+}
+
+getServerUsers();
+
+function renderUsers () {
+    usersOutput.innerHTML = "";
+
+    users.forEach((user) => {
+        usersOutput.innerHTML += `
+            <button class="user-todos-button">${user.name}</button>
+        `
+    });
 }
