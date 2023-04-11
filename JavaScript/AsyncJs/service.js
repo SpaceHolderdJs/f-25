7da7a8707fcec0991fe3076a036223149edd976d
Igor <sergienko339@gmail.com>
2023-04-11 22:05:39 +0300

    recursion

--- JavaScript/AsyncJs/service.js
+++ JavaScript/AsyncJs/service.js
@@ -1,4 +1,9 @@
 class Service {
+  static headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
   constructor(url) {
     this.url = url;
   }
@@ -11,6 +16,7 @@ class Service {
 
   async postRequest(data) {
     const response = await fetch(this.url, {
+      headers: Service.headers,
       method: "POST",
       body: JSON.stringify(data),
     });
@@ -21,6 +27,7 @@ class Service {
 
   async patchRequest(id, data) {
     const response = await fetch(`${this.url}/${id}`, {
+      headers: Service.headers,
       method: "PATCH",
       body: JSON.stringify(data),
     });
@@ -34,47 +41,101 @@ class Service {
       method: "DELETE",
     });
 
-    const status = response.status;
-    return status;
+    const payload = await response.json();
+    return payload;
   }
 }
 
-class PostServise extends Service {
+// class PostServise extends Service {
+//   constructor(url) {
+//     super(url);
+//   }
+// }
+
+// const postService = new PostServise(
+//   "https://jsonplaceholder.typicode.com/posts"
+// );
+
+// postService.getRequest().then((data) => console.log(data, "posts"));
+// postService
+//   .postRequest({ title: "title", body: "string", userId: 0 })
+//   .then((data) => console.log(data, "creation of posts"));
+// postService
+//   .patchRequest(1, { id: 1, title: "title", body: "body" })
+//   .then((data) => console.log(data, "updates of posts"));
+// postService
+//   .deleteRequest(1)
+//   .then((status) => console.log("status of deletion", status));
+
+// class UserService extends Service {
+//   constructor(url) {
+//     super(url);
+//   }
+// }
+
+// const userService = new Service("https://jsonplaceholder.typicode.com/users");
+
+// userService.getRequest().then((data) => console.log(data, "users"));
+// userService
+//   .postRequest({ title: "title", body: "string", userId: 0 })
+//   .then((data) => console.log(data, "creation of users"));
+// userService
+//   .patchRequest(1, { id: 1, title: "title", body: "body" })
+//   .then((data) => console.log(data, "updates of users"));
+// userService
+//   .deleteRequest(1)
+//   .then((status) => console.log("status of deletion (user)", status));
+
+// ----------------------------------------------------
+class UserRealService extends Service {
   constructor(url) {
     super(url);
   }
 }
 
-const postService = new PostServise(
-  "https://jsonplaceholder.typicode.com/posts"
+const realUserService = new UserRealService(
+  "https://learning-server.onrender.com/users"
 );
 
-postService.getRequest().then((data) => console.log(data, "posts"));
-postService
-  .postRequest({ title: "title", body: "string", userId: 0 })
-  .then((data) => console.log(data, "creation of posts"));
-postService
-  .patchRequest(1, { id: 1, title: "title", body: "body" })
-  .then((data) => console.log(data, "updates of posts"));
-postService
-  .deleteRequest(1)
-  .then((status) => console.log("status of deletion", status));
+// realUserService
+//   .postRequest({
+//     name: "Igor10",
+//     age: 23,
+//     location: "Kyiv",
+//     salary: 3000,
+//     skills: ["HTML", "CSS"],
+//   })
+//   .then((data) => {
+//     console.log("new user", data);
+//   });
 
-class UserService extends Service {
-  constructor(url) {
-    super(url);
-  }
-}
+// realUserService.getRequest().then((data) => console.log("users2", data));
 
-const userService = new Service("https://jsonplaceholder.typicode.com/users");
+// realUserService
+//   .patchRequest("6435961868dc217913867831", {
+//     age: 10,
+//     skills: ["HTML", "CSS", "JavaScript"],
+//   })
+//   .then((data) => console.log("updated user", data))
+//   .catch((e) => console.log(e));
 
-userService.getRequest().then((data) => console.log(data, "users"));
-userService
-  .postRequest({ title: "title", body: "string", userId: 0 })
-  .then((data) => console.log(data, "creation of users"));
-userService
-  .patchRequest(1, { id: 1, title: "title", body: "body" })
-  .then((data) => console.log(data, "updates of users"));
-userService
-  .deleteRequest(1)
-  .then((status) => console.log("status of deletion (user)", status));
+// realUserService
+//   .deleteRequest("6435965968dc217913867856")
+//   .then((data) => console.log("deletion data", data))
+//   .catch((e) => console.log("!!!!", e));
+
+// 1.  Написати логіку що прибере повторюваних користувачів за name з загального масиву користувачів
+
+realUserService.getRequest().then((data) => {
+  console.log("users before", data.users);
+
+  const result = [];
+
+  data.users.forEach((user, index, array) => {
+      // const usersWithSameName = array.filter((u) => u.name === user.name);
+      const isUserAdded = result.find((u) => (u.name === user.name) && (u.age === user.age));
+      !isUserAdded && result.push(user);
+  });
+
+  console.log(result, "result");
+});
