590c3786766f6bcba21ae06842a1f86ff7833140
Igor <sergienko339@gmail.com>
2023-06-20 22:09:40 +0300

    angular l2

--- Angular/my-first-project/src/app/app.component.ts
+++ Angular/my-first-project/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title: string = 'my-first-project';
+
+  obj = { city: "Kyiv", lat: 1922982923, lon: 29339393939 }
+  numbers: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  users = [{ name: "Igor", age: 23 }, { name: "Oleg", age: 23 }, { name: "Nastya", age: 23 }, { age: "100" }];
+
+  counter: number = 0;
+
+  iterate() {
+    this.users.forEach((user) => {
+      user.name?.slice()
+    })
+  }
+
+  // mutable !!!
+  increment() {
+    this.counter = this.counter + 1;
+  }
+
+  decrement() {
+    this.counter = this.counter - 1;
+  }
 }
+
