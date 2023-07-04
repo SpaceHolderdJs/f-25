14ced7d591e666604e72c8a004e9898b745b1366
Igor <sergienko339@gmail.com>
2023-07-04 20:46:00 +0300

    tmp An l 4

--- Angular/my-first-project/src/app/users/users.component.ts
+++ Angular/my-first-project/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserInterface } from 'src/types';
 
 @Component({
@@ -6,37 +6,24 @@ import { UserInterface } from 'src/types';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
   searchValue: string = '';
   searchResult: Array<UserInterface> = [];
 
-  users: Array<UserInterface> = [
-    {
-      name: 'Igor',
-      age: 23,
-      locations: [
-        { lat: 127271271, lon: 1212121, title: 'Kyiv' },
-        { lat: 127271271, lon: 23222322, title: 'Poltava' },
-      ],
-    },
-    {
-      name: 'Oleg',
-      age: 63,
-      locations: [{ lat: 127271271, lon: 1212121, title: 'Lviv' }],
-    },
-    {
-      name: 'Nastya',
-      age: 63,
-      locations: [{ lat: 127271271, lon: 1212121, title: 'Lviv' }],
-    },
-  ];
+  ngOnInit() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => this.users = json);
+  }
+
+  users: Array<UserInterface> = [];
 
   onInput(event: any) {
-
     this.searchValue = event.target.value;
 
-    this.searchResult = this.users.filter((user) =>
-      user.name.includes(event.target.value)
+    this.searchResult = this.users.filter(
+      (user) =>
+        user.name.includes(event.target.value)
     );
 
     console.log('Input', event.target.value, this.searchResult);
