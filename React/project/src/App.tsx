ff65834ef3a49b6ea538f8b4b3f9b4db2c93c44c
Igor <sergienko339@gmail.com>
2023-07-20 22:08:20 +0300

    lr3

--- React/project/src/App.tsx
+++ React/project/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { Counter } from './components/Counter';
-import { Users } from './components/Users';
+import React, { useState } from "react";
+import { Counter } from "./components/Counter";
+import { Users } from "./components/Users";
+import { MainContextProvider } from "./contexts/MainContext";
+import { Posts } from "./components/Posts";
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
+  const [a, setA] = useState(100000000);
   return (
     <div className="App">
-      <Counter />
-      <Users />
+      <MainContextProvider a={10}>
+        {/* props */}
+        <Counter
+          value="VALUE"
+          name="Igor"
+          age={10}
+          arr={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          fn={() => console.log("Hello")}
+          abc={a}
+          setA={setA}
+        />
+        <Users />
+        <Posts />
+      </MainContextProvider>
     </div>
   );
 }
--- React/project/src/components/Counter.tsx
+++ React/project/src/components/Counter.tsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState, Dispatch, FC } from "react";
+import { MainContext } from "../contexts/MainContext";
 
-export const Counter = () => {
+interface PropsInterface {
+  value: string;
+  name: string;
+  age: number;
+  arr: number[];
+  abc: number;
+  setA: Dispatch<number>
+  fn: () => void;
+}
+
+export const Counter: FC<PropsInterface> = (props: PropsInterface) => {
+  console.log(props.value, props.name, props.age, props.arr, props.abc, props.setA);
+
+  props.fn();
+
+  const {renderPosts} = useContext(MainContext)!;
+ 
   const [counter, setCounter] = useState<number>(0);
   const [isCounterEnabled, setIsCounterEnabled] = useState<boolean>(true);
+  const [name, setName] = useState<string>("name");
+
+  const names: string[] = ["name1", "name2", "name3", "Igor"];
 
   console.log(counter);
 
@@ -13,8 +33,18 @@ export const Counter = () => {
   const toggleCounterAvailability = () =>
     setIsCounterEnabled(!isCounterEnabled);
 
+  const changeName = (newName: string) => setName(newName);
+
   return (
     <div className="counter">
+      <h3>{name} {props.age}</h3>
+      <div>
+        {names.map((name, i) => (
+          <button key={`${name}-${i}`} onClick={() => changeName(name)}>
+            {name}
+          </button>
+        ))}
+      </div>
       <button onClick={toggleCounterAvailability}>
         {isCounterEnabled ? "Hide" : "Show"} counter
       </button>
@@ -28,6 +58,8 @@ export const Counter = () => {
       {counter < 10 && <button onClick={() => onClickButton(true)}>+</button>}
 
       <button onClick={() => onClickButton(false)}>-</button>
+
+      {renderPosts()}
     </div>
   );
 };
