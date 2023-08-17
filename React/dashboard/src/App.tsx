450b928ca0945543ad4bf055c9446f3f2d715667
Igor <sergienko339@gmail.com>
2023-08-17 22:03:22 +0300

    react + rounting

--- React/dashboard/src/App.tsx
+++ React/dashboard/src/App.tsx
@@ -4,17 +4,19 @@ import "./App.css";
 
 import { FlexColumn } from "./components/shared/Flex";
 import { Auth } from "./components/static/Auth";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { DashboardContextProvider } from "./contexts/DashboardContext";
 
 function App() {
   return (
-    <FlexColumn
-      width="100%"
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-    >
-     <Auth />
-    </FlexColumn>
+      <FlexColumn
+        width="100%"
+      >
+        <DashboardContextProvider>
+          <RouterProvider router={router} />
+        </DashboardContextProvider>
+      </FlexColumn>
   );
 }
 
