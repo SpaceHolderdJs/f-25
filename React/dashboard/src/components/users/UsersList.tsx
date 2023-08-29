63ed3ae593e8a0f9cd340c6503f6679c45b46f54
Igor <sergienko339@gmail.com>
2023-08-29 22:24:42 +0300

    react l5

--- React/dashboard/src/components/users/UsersList.tsx
+++ React/dashboard/src/components/users/UsersList.tsx
@@ -1,24 +1,21 @@
-import React, { useMemo } from "react";
+import React, { useContext, useMemo } from "react";
 import { useFetchData } from "../../hooks/useFetchData";
 import { DashboardUserInterface } from "./types";
 import { FlexColumn } from "../shared/Flex";
 import { UserItem } from "./UserItem";
+import { Search } from "../shared/Search";
+import { SearchContext } from "../../contexts/SearchContext";
 
 export const UsersList = () => {
+  const { users, userSearchResults } = useContext(SearchContext)!;
 
-  const options = useMemo(() => ({ method: "GET" }), []);
-
-  const { data, isLoading } = useFetchData<DashboardUserInterface[]>(
-    "https://jsonplaceholder.typicode.com/users",
-    options
-  );
-
-  console.log(data, isLoading);
+  console.log(userSearchResults, "search of users");
 
   return (
     <FlexColumn width="100%" alignItems="center">
       <h1>Users</h1>
-      {data?.map((user) => (
+      <Search />
+      {users?.map((user) => (
         <UserItem user={user} />
       ))}
     </FlexColumn>
